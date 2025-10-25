eab1696ff12d7b126332f9c208e2c02ca0a89b39
Ivan Nur Ilham Syah <ivannurilham60@gmail.com>
2025-10-25 15:59:58 +0700

    feat: create tooltip controller

---
+++ Sources/EasyTooltip/Controllers/EasyTooltipController.swift
@@ -0,0 +1,68 @@
+//
+//  EasyTooltipController.swift
+//  EasyTooltip
+//
+//  Created by Ivan Nur Ilham Syah on 25/10/25.
+//
+
+import Foundation
+import SwiftUI
+
+final class EasyTooltipController: ObservableObject {
+  @Published var isPresented = false
+  @Published var activeTargetID: AnyHashable? = nil
+
+  private var pendingID: AnyHashable? = nil
+
+  func tapTarget(_ id: AnyHashable) {
+    if activeTargetID == id {
+      dismiss()
+      return
+    }
+    
+    show(id)
+  }
+  
+  func show(_ id: AnyHashable) {
+    activeTargetID = nil
+    
+    guard isPresented else {
+      activeTargetID = id
+      isPresented = true
+      return
+    }
+    
+    pendingID = id
+    isPresented = false
+
+    DispatchQueue.main.async { [weak self] in
+      guard let self = self, let next = self.pendingID else { return }
+      self.activeTargetID = next
+      self.isPresented = true
+      self.pendingID = nil
+    }
+  }
+
+  func dismiss() {
+    pendingID = nil
+    isPresented = false
+  }
+}
+
+
+private struct EasyTooltipControllerKey: EnvironmentKey {
+  static let defaultValue: EasyTooltipController = .init()
+}
+
+extension EnvironmentValues {
+  var tooltipController: EasyTooltipController {
+    get { self[EasyTooltipControllerKey.self] }
+    set { self[EasyTooltipControllerKey.self] = newValue }
+  }
+}
+
+extension View {
+  func tooltipController(_ controller: EasyTooltipController) -> some View {
+    environment(\.tooltipController, controller)
+  }
+}
